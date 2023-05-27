fc51c0e2e3d61ff664cacab2f97a34594724399f
Alejandro Bonfilio <alejandrobonfilio@gmail.com>
2023-05-26 21:13:35 -0300

    desafio2

--- ProductManager.js
+++ ProductManager.js
@@ -1,94 +1,140 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-      this.nextId = 1;
-    }
-  
-    addProduct(product) {
-      if (!this.validateProduct(product)) {
-        console.log("No se pudo agregar el producto. Faltan campos obligatorios o el código está duplicado.");
-        return;
-      }
-  
-      product.id = this.nextId;
-      this.nextId++;
-      this.products.push(product);
-      console.log("Producto agregado correctamente.");
-    }
-  
-    removeProduct(productId) {
-      this.products = this.products.filter((product) => product.id !== productId);
-    }
-  
-    getProductById(productId) {
-      const product = this.products.find((product) => product.id === productId);
-      if (!product) {
-        console.log("Error: Producto no encontrado.");
-      }
-      return product;
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    validateProduct(product) {
-      const { title, description, price, thumbnail, code, stock } = product;
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        return false;
-      }
-  
-      const isDuplicateCode = this.products.some((p) => p.code === code);
-      if (isDuplicateCode) {
-        return false;
-      }
-  
-      return true;
-    }
+  constructor() {
+    this.products = [];
+    this.nextId = 1;
   }
-  
-  class Product {
-    constructor(title, description, price, thumbnail, code, stock) {
-      this.title = title;
-      this.description = description;
-      this.price = price;
-      this.thumbnail = thumbnail;
-      this.code = code;
-      this.stock = stock;
-      this.id = null;
+
+  addProduct(productData) {
+    const { title, description, price, thumbnail, code, stock } = productData;
+
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.log('No se pudo agregar el producto. Faltan campos obligatorios.');
+      return;
     }
+
+    const isDuplicateCode = this.products.some((product) => product.code === code);
+    if (isDuplicateCode) {
+      console.log('No se pudo agregar el producto. El código está duplicado.');
+      return;
+    }
+
+    const product = {
+      id: this.nextId,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock
+    };
+
+    this.products.push(product);
+    this.nextId++;
+    console.log('Producto agregado correctamente.');
+
+    // Guardar los productos en un archivo después de agregar uno nuevo
+    this.saveProductsToFile();
   }
-  
-  // Crear una instancia de ProductManager
-  const manager = new ProductManager();
-  
-  // Agregar productos
-  const product1 = new Product(
-    "Teclado",
-    "Mecanico",
-    10000,
-    "img1.jpg",
-    "101",
-    16
-  );
-  const product2 = new Product(
-    "Mouse",
-    "Razer 1500dpi",
-    20000,
-    "img2.jpg",
-    "102",
-    15
-  );
-  manager.addProduct(product1);
-  manager.addProduct(product2);
-  
-  // Obtener un producto por su ID y mostrarlo en la consola
-  const product = manager.getProductById(1);
-  console.log(product);
-  
-  // Intentar obtener un producto con un ID no existente
-  const nonExistentProduct = manager.getProductById(3);
-  
-  // Obtener todos los productos y mostrarlos en la consola
-  const allProducts = manager.getProducts();
-  console.log(allProducts);
+
+  deleteProduct(productId) {
+    const productIndex = this.products.findIndex((product) => product.id === productId);
+    if (productIndex === -1) {
+      console.log('Error: Producto no encontrado.');
+      return;
+    }
+
+    this.products.splice(productIndex, 1);
+    console.log('Producto eliminado correctamente.');
+
+    // Guardar los productos en un archivo después de eliminar uno
+    this.saveProductsToFile();
+  }
+
+  getProductById(productId) {
+    const product = this.products.find((product) => product.id === productId);
+    if (!product) {
+      console.log('Error: Producto no encontrado.');
+    }
+    return product;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  updateProduct(productId, updatedFields) {
+    const productIndex = this.products.findIndex((product) => product.id === productId);
+    if (productIndex === -1) {
+      console.log('Error: Producto no encontrado.');
+      return;
+    }
+
+    const product = this.products[productIndex];
+    this.products[productIndex] = { ...product, ...updatedFields };
+    console.log('Producto actualizado correctamente.');
+
+    // Guardar los productos en un archivo después de actualizar uno
+    this.saveProductsToFile();
+  }
+
+  saveProductsToFile() {
+    const data = JSON.stringify(this.products, null, 2);
+    fs.writeFile('products.json', data, (err) => {
+      if (err) {
+        console.error('Error al guardar los productos en el archivo:', err);
+      } else {
+        console.log('Productos guardados correctamente en el archivo.');
+      }
+    });
+  }
+
+  loadProductsFromFile() {
+    fs.readFile('products.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error al cargar los productos desde el archivo:', err);
+      } else {
+        try {
+          const products = JSON.parse(data);
+          this.products = products;
+          this.nextId = this.getNextId(products);
+          console.log('Productos cargados correctamente desde el archivo.');
+        } catch (error) {
+          console.error('Error al parsear los datos del archivo JSON:', error);
+        }
+      }
+    });
+  }
+
+  getNextId(products) {
+    let maxId = 0;
+    for (const product of products) {
+      if (product.id > maxId) {
+        maxId = product.id;
+      }
+    }
+    return maxId + 1;
+  }
+}
+class Product {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+    this.id = null;
+  }
+}
+
+// Crear una instancia de ProductManager
+const manager = new ProductManager();
+
+// Cargar productos desde el archivo al iniciar la aplicación
+manager.loadProductsFromFile();
+
+// Obtener todos los productos y mostrarlos en la consola
+const allProducts = manager.getProducts();
+console.log(allProducts);
